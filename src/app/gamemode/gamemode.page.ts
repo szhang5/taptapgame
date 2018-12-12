060ac69a0a51419d3bf668a3456060e74bed8d89
shiyun zhang <shiyunzhang@shiyuns-MacBook-Pro.local>
2018-12-12 14:13:37 -0500

    add tap movement

--- src/app/gamemode/gamemode.page.ts
+++ src/app/gamemode/gamemode.page.ts
@@ -10,6 +10,8 @@ import 'hammerjs';
 })
 export class GamemodePage implements OnInit {
 	public tap: number = 0;
+	public margin: string = '0%';
+	public size: number = 1;
 
   constructor(public alertController: AlertController, private router: Router) { }
 
@@ -18,36 +20,49 @@ export class GamemodePage implements OnInit {
   }
 
   async presentAlert() {
-	    const alert = await this.alertController.create({
-	      header: 'You Win!',
-	      buttons: [{
-          text: 'Restart',
-          cssClass: 'secondary',
-          handler: () => {
-            this.tap = 0;
-          }
-        }, {
-          text: 'Okay',
-          handler: () => {
-          	this.tap = 0;
-            this.backToHome();
-          }
-        }]
-	    });
+    const alert = await this.alertController.create({
+      header: 'You Win!',
+      buttons: [{
+        text: 'Restart',
+        cssClass: 'secondary',
+        handler: () => {
+          this.restart();
+        }
+      }, {
+        text: 'Okay',
+        handler: () => {
+        	this.restart();
+          this.backToHome();
+        }
+      }],
+      backdropDismiss: false
+    });
 
-	    await alert.present();
+    await alert.present();
 	}
 
   tapEvent(e) {
   	this.tap++;
-  	if(this.tap > 30){
-    	this.tap--;
-    }
-    if(this.tap == 30){
+  	this.margin = `${parseInt(this.margin.split('%')[0], 0) + 3}%`;
+  	this.size = this.size + 0.1;
+  	// console.log(this.margin);
+  	// console.log(parseInt(this.margin, 0) );
+  	// console.log(this.size);
+
+  	// if(this.tap > 48){
+   //  	this.restart();
+   //  }
+    if(this.tap == 48){
     	this.presentAlert();
     }
     
     // console.log(this.tap);
+  }
+
+  restart(){
+  	this.tap = 0;
+  	this.margin = '0%';
+  	this.size = 1;
   }
 
   backToHome(){
